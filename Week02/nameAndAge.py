4edf5cf7f764485b31969b63617589c5dadb4e59
Tosin <arabao@tcd.ie>
2022-01-31 23:23:31 +0000

    Week02

--- Week02/nameAndAge.py
+++ Week02/nameAndAge.py
@@ -1,8 +1,8 @@
 # program that reads in name and age
 # Author: Tosin Araba
 
-name = input ('enter your name (\t)')
-print ('Hello' + name)
+name = input ('enter your name (\t) ')
 
 age = input('what is your age?:')
-print('your age is {}'.format(age))
+
+print ('Hello ' + name + '  Your age is', format(age)) 
