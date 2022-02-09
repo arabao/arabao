1b2c0f2c569d761447c5ed3fc1a87b153fdc7c38
Tosin <arabao@tcd.ie>
2022-02-09 23:57:31 +0000

    .\week03

---
+++ Week03/lab3.1.2-sub.py
@@ -0,0 +1,11 @@
+# program that subtracts one number from another
+# author: Tosin Araba
+
+# input reads in a string so we need to convert it into an int
+# so we can perform mathematical operations
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+answer= x - y
+print("{} minus {} is {} ".format(x, y, answer))
+
---
+++ Week03/lab3.2.3-floor.py
@@ -0,0 +1,9 @@
+# floors a number
+# Author: Tosin Araba
+
+
+import math
+
+numberTofloor = float(input("Enter a float number:"))
+flooredNumber = math.floor(numberTofloor)
+print('{} floor is {}'.format(numberTofloor, flooredNumber))
